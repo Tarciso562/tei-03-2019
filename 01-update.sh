8660de6f0fa1c59cb4b85c7babf6e90f4949387e
Tarciso562 <58092794+Tarciso562@users.noreply.github.com>
2020-11-17 20:47:46 -0300

    Add files via upload

--- 01-update.sh
+++ 01-update.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 clear
+
 echo -e "Atualizando as listas do Apt"
-apt update
+    apt update
+
 echo -e "Atualizando os software"
-apt -y upgrade
+    apt -y upgrade
+
 echo -e "Forçando a atualização do sistema"
-apt -y full-upgrade
+    apt -y full-upgrade
+
 echo -e "Forçando a atualização do sistema"
-apt -y dist-upgrade
+    apt -y dist-upgrade
+
 echo -e "Removendo software desnecessários"
-Apt -y autoremove
-apt -y autoclean
-apt  clean
+
+    Apt -y autoremove
+    apt -y autoclean
+    apt  clean
 echo -e "Sistema atualizado, pressione <ENTER> para o reboot"
 read
 sleep 5 seg.
